ffd63a2ab8e29cfc0bff5abd8b8b84ed5639fd5c
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-06-04 17:00:06 -0500

    resolve option references

--- scripts/builder/category.py
+++ scripts/builder/category.py
@@ -1,7 +1,17 @@
 from typing import NotRequired, TypedDict, Unpack
 
+from .options import ToggleOption
+
+
+type OptionInput = str | ToggleOption
+
 
 class CategoryArgs(TypedDict):
+    hidden: NotRequired[bool]
+    yaml_option: NotRequired[OptionInput | list[OptionInput]]
+
+
+class CategoryData(TypedDict):
     hidden: NotRequired[bool]
     yaml_option: NotRequired[list[str]]
 
@@ -11,11 +21,26 @@ type CategoryInput = str | Category
 
 class Category:
     name: str
-    data: CategoryArgs
+    data: CategoryData
 
     def __init__(self, name: str, **kwargs: Unpack[CategoryArgs]) -> None:
+        yaml_option = kwargs.pop("yaml_option", [])
+
         self.name = name
         self.data = {**kwargs}
+
+        self.resolve_yaml_option_input(yaml_option)
+
+    def resolve_yaml_option_input(self, input: OptionInput | list[OptionInput]):
+        if isinstance(input, str):
+            self.data["yaml_option"] = self.data.get("yaml_option") or []
+            self.data["yaml_option"].append(input)
+        elif isinstance(input, ToggleOption):
+            self.data["yaml_option"] = self.data.get("yaml_option") or []
+            self.data["yaml_option"].append(input.name)
+        else:
+            for input_item in input:
+                self.resolve_yaml_option_input(input_item)
 
     @staticmethod
     def from_input(input: CategoryInput):
