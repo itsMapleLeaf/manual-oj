93b23d2a02bb5803bb2590794abdc7d623353ef3
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-06-11 19:11:43 -0500

    internal requires refactor

--- scripts/builder/location.py
+++ scripts/builder/location.py
@@ -1,42 +1,71 @@
-import dataclasses
+from dataclasses import dataclass
 from typing import NotRequired, Optional, TypedDict, Unpack
 from .item import Item
 from .category import Category
 
 
-type RequirementInput = str | Item | Category | AmountRequirement
+class RequirementNode:
+    def to_string(self):
+        raise NotImplementedError()
 
 
-@dataclasses.dataclass
-class AmountRequirement:
-    subject: Item | Category
-    amount: str
-
-
-def some_of(subject: Item | Category, amount: int | str):
-    return AmountRequirement(
-        subject=subject,
-        amount=amount if isinstance(amount, str) else str(amount),
-    )
-
-
-class Requirement:
-    @staticmethod
-    def from_input(input: RequirementInput) -> str:
-        if isinstance(input, str):
-            return input
-
-        if isinstance(input, AmountRequirement):
-            subject = Requirement.__format_subject(input.subject)
-            return f"|{subject}:{input.amount}|"
-
-        return f"|{Requirement.__format_subject(input)}|"
+@dataclass
+class RequirementSubject(RequirementNode):
+    name: str
+    amount: Optional[str | int] = None
+    is_category: bool = False
 
     @staticmethod
-    def __format_subject(input: Item | Category) -> str:
-        if isinstance(input, Category):
-            return f"@{input.name}"
-        return input.name
+    def normalize(input: Item | Category, amount: str | int | None = None):
+        return RequirementSubject(
+            input.name, amount=amount, is_category=isinstance(input, Category)
+        )
+
+    def to_string(self):
+        result = "|"
+
+        if self.is_category:
+            result += "@"
+
+        result += self.name
+
+        if self.amount != None:
+            result += ":" + str(self.amount)
+
+        result += "|"
+
+        return result
+
+
+@dataclass
+class RequirementBinaryExpression(RequirementNode):
+    children: list[RequirementNode]
+    operator: str
+
+    def to_string(self):
+        infix = f" {self.operator} "
+        members = infix.join(node.to_string() for node in self.children)
+        return f"({members})"
+
+
+type RequirementInput = str | Item | Category | RequirementNode
+
+
+def serialize_requirement(input: RequirementInput) -> str:
+    if isinstance(input, str):
+        return input
+
+    if isinstance(input, Item):
+        return RequirementSubject(input.name).to_string()
+
+    if isinstance(input, Category):
+        return RequirementSubject(input.name, is_category=True).to_string()
+
+    return input.to_string()
+
+
+def some_of(subject: Item | Category, amount: str | int):
+    return RequirementSubject.normalize(subject, amount)
 
 
 class LocationBase(TypedDict):
@@ -72,4 +101,4 @@ class Location:
             self.data["category"] = Category.from_list_input(category)
 
         if requires:
-            self.data["requires"] = Requirement.from_input(requires)
+            self.data["requires"] = serialize_requirement(requires)
